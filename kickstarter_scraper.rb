5c04aa296ec5d5eb69f2f6999e1608ad53a616af
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-29 17:03:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
-    
+    projects[project] = {}
   end
   # projects: kickstarter.css("li.project.grid_4")
   # titles: project.css("h2.bbcard_name strong a").text
